3bcb4125d32fd2dce98842acf190c2fbd3fbdd54
Cathella <ncathie214@gmail.com>
2021-05-04 18:32:14 +0300

    add if statements to differ tax

--- main.rb
+++ main.rb
@@ -1,14 +1,28 @@
 basic_sales_tax = 0.1
 import_sales_tax = 0.05
-total_sales_tax = 0
-total_price = 0
+total_sales_tax = 0.0
+total_price = 0.0
 
-File.open("input_1.dat") do |f|
+File.open("input_3.dat") do |f|
   while item = f.gets
     quantity, product, price = item.chomp.split(', ')
-    tax = (basic_sales_tax * price.to_f).round(2)
+
+    if product.include? "book"
+      tax = 0.0
+    elsif product.include? "chocolate"
+      tax = 0.0
+    elsif product.include? "pills"
+      tax = 0.0
+    elsif product.include? "imported"
+      tax = ((basic_sales_tax + import_sales_tax) * price.to_f).round(2)
+    else
+      tax = (basic_sales_tax * price.to_f).round(2)
+    end
+
     price_after_tax = (tax + price.to_f).round(2)
-    puts "#{quantity}, #{product}, price: #{price_after_tax}"
+
+    puts "#{quantity}, #{product}, #{price_after_tax}"
+
     total_sales_tax += tax
     total_price += price_after_tax
   end
