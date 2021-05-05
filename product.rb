ed1289abafcf91d5593f8974225240852d89e4c1
Cathella <ncathie214@gmail.com>
2021-05-05 13:27:09 +0300

    creating method actions

--- product.rb
+++ product.rb
@@ -1,17 +1,3 @@
 class Product
-  def is_food?(product)
-    product.include? 'chocolate'
-  end
-
-  def is_medicine?(product)
-    product.include? 'pills'
-  end
-
-  def is_book?(product)
-    product.include? 'book'
-  end
-
-  def is_imported?(product)
-    product.include? 'imported'
-  end
+  
 end
--- tax.rb
+++ tax.rb
@@ -1,16 +1,3 @@
 class Tax
-  basic_sales_tax = 0.1
-  import_sales_tax = 0.05
-
-  def sales_tax(price)
-    basic_sales_tax * price
-  end
-
-  def tax_on_imported_products(price)
-    import_sales_tax * price
-  end
-
-  def tax_on_non_taxed_products(price)
-    0.0
-  end
+  
 end
